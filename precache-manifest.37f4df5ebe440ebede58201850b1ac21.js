5f468819faae990a826b5500060e819d68cf9ef2
Tom Brow <tom@tombrow.com>
2020-01-18 13:47:14 -0600

    Updates

---
+++ precache-manifest.37f4df5ebe440ebede58201850b1ac21.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "194577a7e20bdcc7afbb718f502c134c",
+    "url": "/Mealplan/.DS_Store"
+  },
+  {
+    "revision": "3fda693f467dc86b567ea734feef536e",
+    "url": "/Mealplan/chota.min.css"
+  },
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/Mealplan/favicon.ico"
+  },
+  {
+    "revision": "93a950004213c2822b4ccefe9c438648",
+    "url": "/Mealplan/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/Mealplan/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/Mealplan/manifest.json"
+  },
+  {
+    "revision": "23deca9c63e00dbebb5b",
+    "url": "/Mealplan/static/css/main.5216141e.chunk.css"
+  },
+  {
+    "revision": "23deca9c63e00dbebb5b",
+    "url": "/Mealplan/static/js/main.23deca9c.chunk.js"
+  },
+  {
+    "revision": "ff9be7f33e5e4f3370cd",
+    "url": "/Mealplan/static/js/runtime~main.ff9be7f3.js"
+  },
+  {
+    "revision": "3ca81432027bbb7b376c",
+    "url": "/Mealplan/static/js/vendors~main.3ca81432.chunk.js"
+  }
+]);
