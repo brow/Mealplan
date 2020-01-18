7de1463e832a29ab9d37bf67ad341dd7c3212de8
Tom Brow <tom@tombrow.com>
2020-01-18 14:27:46 -0600

    config for deploying to GH Pages

---
+++ elmapp.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    // For deploying to GitHub Pages:
+    // https://github.com/halfzebra/create-elm-app/blob/4dd774816cebdb440f4d2f161a16c8dad41986a9/template/README.md#step-1-add-homepage-to-elmappconfigjs
+    homepage: "https://brow.github.io/Mealplan",
+}
