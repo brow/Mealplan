e6d5593c901707dc0989c6fda8c9276ba0b4bb0c
Tom Brow <tom@tombrow.com>
2020-01-18 17:43:08 -0600

    begin SPA

--- src/Main.elm
+++ src/Main.elm
@@ -1,11 +1,84 @@
 module Main exposing (main)
 
+import Browser
+import Browser.Navigation
 import Shopper
+import Url
+
+
+type Hole
+    = Hole
 
 
 
----- PROGRAM ----
+-- MAIN
 
 
+main : Program () Model Msg
 main =
-    Shopper.main
+    Browser.application
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = always Sub.none
+        , onUrlRequest = always Noop
+        , onUrlChange = always Noop
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { key : Browser.Navigation.Key
+    , page : Page
+    }
+
+
+type Page
+    = Plan
+    | Shop
+
+
+
+-- VIEW
+
+
+view : Model -> Browser.Document msg
+view model =
+    { title =
+        case model.page of
+            Plan ->
+                "Plan"
+
+            Shop ->
+                "Shop"
+    , body = []
+    }
+
+
+
+-- INIT
+
+
+init : () -> Url.Url -> Browser.Navigation.Key -> ( Model, Cmd msg )
+init _ url key =
+    ( { key = key, page = Plan }
+    , Cmd.none
+    )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = Noop
+
+
+update : Msg -> Model -> ( Model, Cmd msg )
+update message model =
+    case message of
+        Noop ->
+            ( model, Cmd.none )
--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import './main.css';
 import { Elm } from './Main.elm';
 import registerServiceWorker from './registerServiceWorker';
 
-var app = Elm.Main.init({
-  node: document.getElementById('root')
-});
-app.ports.error.subscribe(function(error) {
-    console.error(error);
-});
+var app = Elm.Main.init();
+
+// app.ports.error.subscribe(function(error) {
+//     console.error(error);
+// });
 
 registerServiceWorker();
