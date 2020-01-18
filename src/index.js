04d181cfc125853538974e0a844591dd3f895f2f
Tom Brow <tom@tombrow.com>
2020-01-18 14:39:08 -0600

    remove service worker stuff

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import './main.css';
 import { Elm } from './Main.elm';
-import registerServiceWorker from './registerServiceWorker';
 
 var app = Elm.Main.init({
   node: document.getElementById('root')
@@ -8,5 +7,3 @@ var app = Elm.Main.init({
 app.ports.error.subscribe(function(error) {
     console.error(error);
 });
-
-registerServiceWorker();
