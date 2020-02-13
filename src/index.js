ff95071bd047798f419c38c2dccdfea1980b88d0
Tom Brow <tom@tombrow.com>
2020-02-13 09:13:58 -0600

    confirmation before leaving page

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import './main.css';
 import { Elm } from './Main.elm';
 import registerServiceWorker from './registerServiceWorker';
 
+window.addEventListener("beforeunload", function (e) {
+  let warning = "Your ingredients and shopping list will be lost when you leave this page.";
+  (e || window.event).returnValue = warning;
+  return warning;
+});
+
 var app = Elm.Main.init();
 
 app.ports.error.subscribe(function(error) {
